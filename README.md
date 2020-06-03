cb4f1c292ec96d264d49c8b0918803887381fe22
Estevão Almeida <e.almeida10@hotmail.com>
2020-06-03 12:51:09 -0300

    Add more infos in README.ms

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # aula_javascript
 Introdução ao JavaScript
+
+Estou testando como funciona o Git e como fazer Commits para o GitHub
+<3
--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,1 @@
-window.console.log("MARIA MEU AMOR");
-console.log(
-    document.getElementById("euro").querySelectorAll(".times")0
-)
+window.console.log("Testando");
